a9405ede755d980e33ad9d1978647abe135cadba
Arun Seetharam <arnstrm@iastate.edu>
2015-08-26 11:08:50 -0500

    added a helper script

---
+++ scores2dokuwiki.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ tr -d '\015' < $1 | \
+ sed 's/_fastqc.zip//g' |\
+ sed 's/,-1/|@red: -1/g' |\
+ sed 's/,0/|@yellow: 0/g' |\
+ sed 's/,0/|@yellow: 0/g' |\
+ sed 's/,1/|@lightgreen: 1/g' |\
+ sed 's/$/|/' |sed 's/^/^/' |\
+ sed 1i'^Fastq file ^BSQ ^TSQ ^SQS ^BSC ^GCc ^BNC ^SLD ^SDL ^ORS ^ADP ^KMR ^'
+
+cat <<EOF
+<code>
+Where:
+BSQ : Per_base_sequence_quality, 
+TSQ : Per_tile_sequence_quality, 
+SQS : Per_sequence_quality_scores, 
+BSC : Per_base_sequence_content, 
+GCc : Per_sequence_GC_content, 
+BNC : Per_base_N_content, 
+SLD : Sequence_Length_Distribution, 
+SDL : Sequence_Duplication_Levels, 
+ORS : Overrepresented_sequences, 
+ADP : Adapter_Content, 
+KMR : Kmer_Content 
+</code>
+EOF
