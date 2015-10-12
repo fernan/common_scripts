b9da75bc8f152ed23eb4b2bd0f1176de36719c6f
Arun Seetharam <arnstrm@iastate.edu>
2015-10-12 14:06:32 -0500

    changes

--- fasta_length.py
+++ fasta_length.py
@@ -18,5 +18,5 @@ else:
     cmdargs = str(sys.argv)
     for seq_record in SeqIO.parse(str(sys.argv[1]), "fasta"):
         output_line = '%s\t%i' % \
-    (seq_record.id, len(seq_record)) 
-    print(output_line)
+        (seq_record.id, len(seq_record)) 
+        print(output_line)
