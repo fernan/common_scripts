ad9f4eca7beefc4e79c90f301794d78e14494ddc
Arun Seetharam <arnstrm@iastate.edu>
2016-06-01 09:06:26 -0500

    auto detect the path and perform the task for ModuleGen.sh

--- ModuleGen.sh
+++ ModuleGen.sh
@@ -2,14 +2,23 @@
 echo "module file generator"
 echo "====================="
 
-while true; do
-    read -p "are you in the pogramdir/version_num location: " yn
-    case $yn in
-        [Yy]* ) version=$(basename $(pwd)); ppath=$(dirname $(pwd)); break;;
-        [Nn]* ) echo "please change to program_dir/version_num first"; exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+cwd=$(dirname $(pwd))
+#echo $cwd
+bwd=$(dirname ${cwd})
+#echo $bwd
+if [[ "$bwd" == "/shared/software/GIF/programs" ]]; then
+echo "Path is correct, proceeding ...."
+
+#while true; do
+#    read -p "are you in the pogramdir/version_num location: " yn
+#    case $yn in
+#        [Yy]* ) version=$(basename $(pwd)); ppath=$(dirname $(pwd)); break;;
+#        [Nn]* ) echo "please change to program_dir/version_num first"; exit;;
+#        * ) echo "Please answer yes or no.";;
+#    esac
+#done
+version=$(basename $(pwd));
+ppath=$(dirname $(pwd));
 pname=$(basename $ppath);
 
 IFS= read -r -p "short description: " des;
@@ -18,6 +27,7 @@ IFS= read -r -p "download url: " durl;
 IFS= read -r -p "info url: " iurl;
 IFS= read -r -p "dir name for adding to path (list them with a space in between eg: utils scripts): " dpname;
 IFS=' ' read -r -a array <<< "$dpname"
+PHOME="$(echo "$pname" | tr '[:lower:]' '[:upper:]')"_HOME
 
 
 mkdir -p /shared/software/GIF/modules/$pname;
@@ -37,6 +47,8 @@ echo -e "prepend-path\tPATH\t\t\t\$progdir/$element";
 done >> /shared/software/GIF/modules/$pname/$version
 if [ -d bin ]; then
 echo -e "prepend-path\tPATH\t\t\t\$progdir/bin" >> /shared/software/GIF/modules/$pname/$version
+else
+echo -e "prepend-path\tPATH\t\t\t\$progdir" >> /shared/software/GIF/modules/$pname/$version
 fi
 if [ -d lib ]; then
 echo -e "prepend-path\tLD_LIBRARY_PATH\t\t\$progdir/lib" >> /shared/software/GIF/modules/$pname/$version
@@ -53,7 +65,7 @@ fi
 if [ -d share ]; then
 echo -e "prepend-path\tMANPATH\t\t\t\$progdir/share/man" >> /shared/software/GIF/modules/$pname/$version
 fi
-
+echo -e "setenv\t$PHOME\t\t\t\$progdir" >> /shared/software/GIF/modules/$pname/$version
 cat <<MODFILE2 >> /shared/software/GIF/modules/$pname/$version
 proc ModulesDisplay { } {
 global version name progdir
@@ -70,6 +82,9 @@ Info: $iurl
 proc ModulesHelp {} { ModulesDisplay }
 MODFILE2
 
-cat /shared/software/GIF/modules/$pname/$version
-
+#cat /shared/software/GIF/modules/$pname/$version
 echo "module file written sucessfully"
+echo "Module file location: /shared/software/GIF/modules/$pname/$version"
+else
+echo "please change to /path/to/programs/program_dir/version_num first";
+fi
