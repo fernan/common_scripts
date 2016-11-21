6bb068aa6d1dea6b163ac3332ec16b204bb0a0bc
Arun Seetharam <aseetharam@users.noreply.github.com>
2016-11-21 14:55:26 -0600

    condo fix2

--- runHISAT2.sh
+++ runHISAT2.sh
@@ -3,7 +3,7 @@
 module load hisat2
 module load samtools
 
-p=31 # number of thereads to use (specify one less from total available for sam2bam conversion)
+p=16 # number of thereads to use (specify one less from total available for sam2bam conversion)
 DATABASE="$3"
 R1_FQ="$1"
 R2_FQ="$2"
@@ -11,7 +11,7 @@ R2_FQ="$2"
 OUTPUT=$(basename ${R1_FQ} |cut -f 1 -d "_");
 
 hisat2 \
-  -p 16 \
+  -p ${p} \
   -x ${DATABASE} \
   -1 ${R1_FQ} \
   -2 ${R2_FQ} | \
