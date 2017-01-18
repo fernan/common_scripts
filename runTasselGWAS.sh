6acbf28a0a7e39963072cc3fcacd493402ab6088
Arun Seetharam <arnstrm@iastate.edu>
2017-01-18 10:28:37 -0600

    To run the genomescope using jellyfish kmers. Note: you need to manually insert all the reads in this script before you execute

--- runTasselGWAS.sh
+++ runTasselGWAS.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 module load tassel
 VCF="$1"
-TRAITS="/data021/GIF/arnstrm/Purcell/albacore/08_Tassel/vcf_files/pheno_traits.txt"
+TRAITS="/data013/GIF/arnstrm/Purcell/albacore/2_gatk/d_tassel/pheno_traits.txt"
+# traits file should be numerical
+# else the file will be empty
 
-${TASSEL_HOME}/run_pipeline.pl -fork1 -importGuess ${VCF} -ImputationPlugin -ByMean true -nearestNeighbors 5 -distance Euclidean -endPlugin -export ${VCF%.*}_imputed -exportType VCF
-
+#sort
+${TASSEL_HOME}/run_pipeline.pl -SortGenotypeFilePlugin -inputFile ${VCF} -outputFile ${VCF%.*}_sorted.vcf
+#impute
+${TASSEL_HOME}/run_pipeline.pl -fork1 -importGuess ${VCF%.*}_sorted.vcf -ImputationPlugin -ByMean true -nearestNeighbors 5 -distance Euclidean -endPlugin -export ${VCF%.*}_imputed -exportType VCF
+# generate kinship matrix
 ${TASSEL_HOME}/run_pipeline.pl -fork2 -importGuess ${VCF%.*}_imputed.vcf -KinshipPlugin -method Centered_IBS -endPlugin -export ${VCF%.*}_kinship.txt -exportType SqrMatrix
-
+# GLM
 ${TASSEL_HOME}/run_pipeline.pl \
      -fork1 \
          -importGuess ${VCF%.*}_imputed.vcf \
@@ -20,5 +25,5 @@ ${TASSEL_HOME}/run_pipeline.pl \
          -input3 \
          -intersect \
          -FixedEffectLMPlugin \
-         -maxP 1.0 -permute true -nperm 1000 -biallelicOnly false \
+         -maxP 1e-3 -permute true -nperm 1000 -biallelicOnly true \
          -endPlugin -export ${VCF%.*}_glm_output
