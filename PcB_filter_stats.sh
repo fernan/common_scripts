c78a9878f92a89ad1772fac881bd538a53e9c08b
Arun Seetharam <arnstrm@iastate.edu>
2017-03-10 10:11:53 -0600

    added script to gather pacbio subread filtering stats from the results files

--- PcB_filter_stats.sh
+++ PcB_filter_stats.sh
@@ -4,6 +4,7 @@ cd ${dir}
 a=$(basename $(dirname $(grep "data ref" input.xml |cut -f 2 -d "\"")))
 b=$(basename $(grep "data ref" input.xml |cut -f 2 -d "\""))
 all=$(grep -i "<tr><td>Polymerase Read" results/filter_reports_filter_stats.html |cut -f 5,7 -d ">" | sed 's/<\/td>/\n/' |cut -f 1 -d "<")
-read -r -a stat <<< "$all"
-echo -e "tar_file\tdir\tsmrtcell\tpre-bases\tpre-reads\tpre-n50\tpre-mean_len\tpre-mean_reads\t-post-bases\post-treads\tpost-n50\tpost-mean_len\tpost-mean_reads"
+stat=( $all )
+echo -e "tar_file\tdir\tsmrtcell\tpre-bases\tpre-reads\tpre-n50\tpre-mean_len\tpre-mean_read_score\tpost-bases\tpost-reads\tpost-n50\tpost-mean_len\tpost-mean_read_score"
 echo -e "unk\t$a\t$b\t${stat[0]}\t${stat[2]}\t${stat[4]}\t${stat[6]}\t${stat[8]}\t${stat[1]}\t${stat[3]}\t${stat[5]}\t${stat[7]}\t${stat[9]}"
+cd ..
