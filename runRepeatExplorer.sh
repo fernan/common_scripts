618b4b371ab453f6ff35fd9b083c2bad6d45ad3a
remkv6 <remkv6@iastate.edu>
2017-11-08 13:33:59 -0600

    fixed missing fasta conversion

--- runRepeatExplorer.sh
+++ runRepeatExplorer.sh
@@ -19,7 +19,10 @@ head -n 2000000 <(zcat $R2) >200kReverse.fastq
 
 java -jar /opt/rit/app/trimmomatic/0.36/bin/trimmomatic-0.36.jar PE -threads 16 -phred33 -trimlog 200k.log 200kForward.fastq 200kReverse.fastq 200kForward.trimmedpaired.fastq 200kForward.trimmedunpaired.fastq 200kReverse.trimmedpaired.fastq 200kReverse.trimmedunpaired.fastq ILLUMINACLIP:/opt/rit/app/trimmomatic/0.36/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:$ReadLength
 
-awk '/^>/{print ">" ++i"f"; next}{print}' 200kForward.trimmedpaired.fastq >Really200kForward.trimmedpairedRenamed.fasta
-awk '/^>/{print ">" ++i"f"; next}{print}' 200kReverse.trimmedpaired.fastq >Really200kReverse.trimmedpairedRenamed.fasta
+awk 'NR % 4 == 1 {print ">" $0 } NR % 4 == 2 {print $0}' 200kForward.trimmedpaired.fastq |head -n 400000 > Really200kForward.trimmedpaired.fasta
+awk 'NR % 4 == 1 {print ">" $0 } NR % 4 == 2 {print $0}' 200kReverse.trimmedpaired.fastq |head -n 400000 > Really200kReverse.trimmedpaired.fasta
+
+awk '/^>/{print ">" ++i"f"; next}{print}' Really200kForward.trimmedpaired.fasta >Really200kForward.trimmedpairedRenamed.fasta
+awk '/^>/{print ">" ++i"f"; next}{print}' Really200kReverse.trimmedpaired.fasta >Really200kReverse.trimmedpairedRenamed.fasta
 
 seqtk mergepe Really200kForward.trimmedpairedRenamed.fasta Really200kReverse.trimmedpairedRenamed.fasta >Really200kReverse.trimmedpairedRenamedInterlaced.fasta
