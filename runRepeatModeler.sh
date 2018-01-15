12ba7cdcd16edac6940413a079f5feeb2c36ab4f
remkv6 <remkv6@iastate.edu>
2018-01-15 11:10:27 -0600

    updated the module locations

--- runRepeatModeler.sh
+++ runRepeatModeler.sh
@@ -16,10 +16,12 @@ fi
 
 GENOME="$1"
 module use /shared/software/GIF/modules/
+module purge
 module load parallel
-module load repeatmasker/4.0.6
-module load repeatmodeler/1.0.8
 
+module load GIF2/repeatmasker/4.0.6
+module load GIF2/repeatmodeler/1.0.8
+module load GIF2/perl/5.22.1
 DATABASE="$(basename ${GENOME%.*}).DB"
 BuildDatabase -name ${DATABASE} -engine ncbi ${GENOME}
 RepeatModeler -database ${DATABASE}  -engine ncbi -pa 16
